3529a4e7de9d04cde7f4e021295b2df06b63a26b
axxra <144186316+axxra@users.noreply.github.com>
2024-05-11 01:28:15 +0530

    Initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: any }) {
         />
       </head>
       <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <MantineProvider theme={theme} defaultColorScheme="auto">{children}</MantineProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,32 @@
-export default function HomePage() {
-  return <div>Home page</div>;
+"use client";
+
+import { ActionIcon, Button, Group, rem, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
+import { IconMoon, IconSun } from "@tabler/icons-react";
+
+export default function IndexPage() {
+
+  const { colorScheme, setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme('dark');
+
+  return (
+    <Group pt={50} justify="center" align="center" style={{ height: '100vh', display: "flex", flexDirection: "column" }}>
+      <Button /* color="myColor"  */ size="xl">Welcome to Mantine!</Button>
+      <Button mt={10} leftSection={computedColorScheme === 'dark' ? <IconSun style={{ width: rem(22), height: rem(22) }} stroke={1.5} /> : <IconMoon style={{ width: rem(22), height: rem(22) }} stroke={1.5} />} size="md" onClick={() => {
+        setColorScheme(computedColorScheme === 'dark' ? 'light' : 'dark');
+      }}>{computedColorScheme === 'dark' ? `Light` : `Dark`} Theme</Button>
+      <ActionIcon
+        mt={10}
+        onClick={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')}
+        variant="default"
+        size="xl"
+        aria-label="Toggle color scheme"
+      >
+        {
+          computedColorScheme === 'dark' ?
+            <IconSun stroke={1.5} style={{ width: rem(22), height: rem(22) }} /> :
+            <IconMoon stroke={1.5} style={{ width: rem(22), height: rem(22) }} />
+        }
+      </ActionIcon>
+    </Group>
+  );
 }
